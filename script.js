0280046d0de901c0db95b59a1287bd2496e216fa
EbvidPro <davidebenezer93@gmail.com>
2022-03-08 13:32:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function Calc(e) {
         document.getElementById("output").style.visibility = "visible";
     } else if (selectedValue === "Kilograms") {
         document.getElementById("gramsOutput").innerHTML = userInput * 1000;
-        document.getElementById("kgOutput").style.display = "none";
+        document.getElementById("kgOutput").innerHTML = userInput;
         document.getElementById("ozOutput").innerHTML = (userInput * 35.274).toFixed(3);
         document.getElementById("output").style.visibility = "visible";
     } else if (selectedValue === "Ouuces") {
         document.getElementById("gramsOutput").innerHTML = (userInput * 28.35).toFixed(3);
         document.getElementById("kgOutput").innerHTML = (userInput / 35.274).toFixed(3);
-        document.getElementById("ozOutput").style.display = "none";
+        document.getElementById("ozOutput").innerHTML = userInput;
         document.getElementById("output").style.visibility = "visible";
     }
     // document.getElementById("gramsOutput").innerHTML = userInput / 0.0022046;
